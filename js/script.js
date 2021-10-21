c384c1bda4553bf029986143c816923f77ffe5de
captblaze <standbymii@yahoo.com>
2021-10-21 10:26:37 +0100

    cipher practice

---
+++ js/script.js
@@ -0,0 +1,4 @@
+let userInput = prompt("write a sentence");
+let firstLetter = userInput.charAt(0);
+let lastLetter = userInput.charAt(userInput.length-1);
+let firstAndLast = firstLetter.concat(lastLetter);
